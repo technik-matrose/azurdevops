a7c4a89ba32d77c624ef366a9bef10b9c5c121e0
technik-matrose <121881667+technik-matrose@users.noreply.github.com>
2023-01-29 21:59:40 +0100

    Update input.tf

--- terraform/modules/vm/input.tf
+++ terraform/modules/vm/input.tf
@@ -1,1 +1,9 @@
+variable "location" {}
 
+variable "application_type" {}
+variable "resource_type" {}
+variable "resource_group" {}
+
+variable "subnet_id" {}
+variable "public_ip_address_id" {} 
+variable "admin_username" {}
